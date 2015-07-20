8f83ddb96d8dd3c5c9b0f9738a246d67438371fd
Lucas Torri <lucastorri@gmail.com>
2015-07-20 22:31:37 +0200

    improve test

--- test.sh
+++ test.sh
@@ -3,10 +3,11 @@
 TEST_BRANCH="test"
 
 
+cd $(dirname "$0")
 git checkout master
 
-if [ `git branch --list $TEST_BRANCH `]; then
-  git branch -D $TEST_BRANCH
+if [ `git branch --list "$TEST_BRANCH"` ]; then
+  git branch -D "$TEST_BRANCH"
 fi
 
 git branch $TEST_BRANCH
@@ -19,3 +20,5 @@ touch builds/{build-added,build-updated,build-same}
 touch .known-builds/{build-updated,build-same,build-removed} 
 
 md5 -q builds/build-same > .known-builds/build-same
+
+./build.sh
